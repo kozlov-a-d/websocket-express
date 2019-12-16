29ef484973c2cd028025cf7507100cf496090667
Andrey Kozlov <andreydmitrievich666@gmail.com>
2019-12-16 22:18:38 +0300

    update

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,51 +1,30 @@
-const config = {
-    url: {
-        protocol: 'ws',
-        host: 'localhost',
-        port: 3020
-    }
+if (!window.WebSocket) {
+	document.body.innerHTML = 'WebSocket в этом браузере не поддерживается.';
 }
-const socket = new WebSocket(`${config.url.protocol}://${config.url.host}:${config.url.port}`);
 
-socket.onopen = function(e) {
-    console.log(`[open] Соединение установлено"`);
-    // Отправляем данные на сервер
-    socket.send(`Меня зовут Username${Math.round(Math.random()*10000)}`);
-};
+// создать подключение
+var socket = new WebSocket("ws://localhost:8081");
 
-socket.onmessage = function(event) {
-    console.log(`[message] Данные получены с сервера: ${event.data}`);
-};
-
-socket.onclose = function(event) {
-    if (event.wasClean) {
-        console.log(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
-    } else {
-        // например, сервер убил процесс или сеть недоступна
-        // обычно в этом случае event.code 1006
-        console.log('[close] Соединение прервано');
-    }
-};
-
-socket.onerror = function(error) {
-    console.log(`[error] ${error.message}`);
-};
-
-//===========================================
-
-// отправка сообщения из формы
+// отправить сообщение из формы publish
 document.forms.publish.onsubmit = function() {
-    let outgoingMessage = this.message.value;
-  
-    socket.send(outgoingMessage);
+    if ( this.message.value !== '' ) {
+        var outgoingMessage = this.message.value;
+        socket.send(outgoingMessage);
+        this.message.value = '';
+    }
     return false;
 };
-  
-// получение сообщения - отобразить данные в div#messages
+
+// обработчик входящих сообщений
 socket.onmessage = function(event) {
-    let message = event.data;
-  
-    let messageElem = document.createElement('div');
-    messageElem.textContent = message;
-    document.getElementById('messages').prepend(messageElem);
+    var incomingMessage = event.data;
+    showMessage(incomingMessage); 
+};
+
+// показать сообщение в div#subscribe
+function showMessage(message) {
+    var messageElem = document.createElement('div');
+    messageElem.classList.add('chat-messages__item');
+    messageElem.appendChild(document.createTextNode(message));
+    document.getElementById('subscribe').appendChild(messageElem);
 }
